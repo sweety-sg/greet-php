1f1bedbcb00e82a4bd1c4bea622b1d8bd67bc4d8
Sweety Gupta <sweety_g@cs.iitr.ac.in>
2021-05-22 19:35:37 +0530

    commented echo

--- validation.php
+++ validation.php
@@ -6,14 +6,14 @@ $pwd = $data -> pwd;
 // $pwd = $_REQUEST['pwd'];
 $sql = "SELECT password FROM sweety_userdata WHERE username=\"" . $username . "\";" ;
 // echo $sql;
-echo $username;
+// echo $username;
 $result = mysqli_query($conn,$sql);
 if($result -> num_rows>0){
     while ($pass = mysqli_fetch_assoc($result)) { 
         foreach ($pass as $password) {
         }
     }
-    echo $pwd . $password;
+    // echo $pwd . $password;
         if($pwd==$password){
             echo 1;
           } 
