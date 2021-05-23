2dbc7e5aaa55309ec789f9f5bcdfa8ed289dede2
Sweety Gupta <sweety_g@cs.iitr.ac.in>
2021-05-24 00:28:24 +0530

    syntax error

--- chatbox.php
+++ chatbox.php
@@ -2,16 +2,16 @@
 include_once "config.php";
 $other = $_COOKIE['sender'];
 $user = $_COOKIE['username'];
-$sql = "SELECT * FROM sweety_chat WHERE (sender=\"" .$sender. "\" OR sender=\"" . $receiver . "\") AND (receiver=\"" .$sender. "\" OR receiver=\"" . $receiver . "\") ORDER BY ig ASC ;" ;
+$sql = "SELECT * FROM sweety_chat WHERE (sender=\"" .$sender. "\" OR sender=\"" . $receiver . "\") AND (receiver=\"" .$sender. "\" OR receiver=\"" . $receiver . "\") ORDER BY id ASC ;" ;
 $result($conn, $sql);
-// function setInterval($f, $seconds)
-// {
-//     while(true)
-//     {
-//         $f();
-//         sleep($seconds);
-//     }
-// }
+function setInterval($f, $seconds)
+{
+    while(true)
+    {
+        $f();
+        sleep($seconds);
+    }
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -30,25 +30,25 @@ $result($conn, $sql);
             <span id="chat-error"></span>
             <div class="chats" >
                 <?php
-                // setInterval(function(){
-                //     while($row = mysqli_fetch_assoc($result) ){
-                //         if($row['sender']==$other){
-                //            echo "<div class=\"receiver\">" .$row['msg'] . "</div>";
-                //         }
-                //         if($row['sender']==$user){
-                //            echo "<div class=\"sender\">" . $row['msg'] . "</div>";
-                //         }
-                //     }
-                // },1);
+                setInterval(function(){
+                    while($row = mysqli_fetch_assoc($result) ){
+                        if($row['sender']==$other){
+                           echo "<div class=\"receiver\">" .$row['msg'] . "</div>";
+                        }
+                        if($row['sender']==$user){
+                           echo "<div class=\"sender\">" . $row['msg'] . "</div>";
+                        }
+                    }
+                },1);
                 ?>
                 <!-- <a href = "javascript: test()">click</a> -->
-                <div class="sender">
+                <!-- <div class="sender">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.
                 </div>
                 <div class="receiver">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut .
                 no idea
-                </div>
+                </div> -->
             </div>
             <div class="message">
     <input type="text" name="msg" id="msg" style="width: 90%; height:40px ; ">
