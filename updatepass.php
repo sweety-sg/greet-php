d9745de5afb1adc69103dda3af6e49b13b69cd8c
Sweety Gupta <sweety_g@cs.iitr.ac.in>
2021-05-24 00:20:44 +0530

    syntax error

--- updatepass.php
+++ updatepass.php
@@ -1,6 +1,7 @@
 <?php
 include_once "config.php";
 $new = $_REQUEST['new'];
+setcookie("password", $new);
 $username = $_COOKIE['username'];
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $sql = "UPDATE sweety_userdata
@@ -11,7 +12,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $res= mysqli_query($conn, $sql);
 if($res){
    echo "<script type='text/javascript'>
-   document.cookie= \"password=\" +" . $new . ";
    location.href = \"users.php\";</script>";
 } else{
     echo "<script type='text/javascript'>alert('Sorry, an error occured');</script>";
